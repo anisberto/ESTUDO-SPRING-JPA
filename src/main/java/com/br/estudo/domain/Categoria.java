aa506f2dbf0823f2be62f8c6d437a12a7f2d21a2
Anisberto Reis <anisbertoos@gmail.com>
2021-02-17 21:54:16 -0300

    Testando classe de dominio

---
+++ src/main/java/com/br/estudo/domain/Categoria.java
@@ -0,0 +1,55 @@
+package com.br.estudo.domain;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+import java.util.Objects;
+
+
+public class Categoria implements Serializable {
+    private static final long serialVersionUID = 1;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+
+    public Categoria() {
+
+    }
+
+    public Categoria(Long id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Categoria categoria = (Categoria) o;
+        return id.equals(categoria.id) && Objects.equals(nome, categoria.nome);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, nome);
+    }
+}
--- src/main/java/com/br/estudo/resources/CategoriaResource.java
+++ src/main/java/com/br/estudo/resources/CategoriaResource.java
@@ -1,15 +1,25 @@
 package com.br.estudo.resources;
 
+import com.br.estudo.domain.Categoria;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
     @RequestMapping(method = RequestMethod.GET)
-    public String listar(){
-        return "REST LISTEN";
+    public List<Categoria> listar(){
+        Categoria cat1 = new Categoria(1L,"Tecnologia");
+        Categoria cat2 = new Categoria(2L,"Literatura");
+
+        List<Categoria> list = new ArrayList<>();
+        list.add(cat1);
+        list.add(cat2);
+        return list;
     }
 }
